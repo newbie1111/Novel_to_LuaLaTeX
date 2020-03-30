e6cbd13a24abe2a527142325dcce7357e8e93496
Takerou <harehare753.gmail.com>
2020-03-30 18:48:23 +0900

    Changed the get_Story method to get_contentsDict method so that it can be referenced by dict type.

--- kakuyomu_LuaLaTeX.py
+++ kakuyomu_LuaLaTeX.py
@@ -5,7 +5,9 @@ import requests
 import pathlib
 import re
 from bs4 import BeautifulSoup
+import time
 import novel_maker
+import formatter
 
 # kakuyomu_LuaLaTeX class
 
@@ -49,55 +51,30 @@ class kakuyomu_LuaLaTeX():
     # get episode link hrefs list
     def get_episodeLinks(self):
         episodes_hrefs = []
-        links = [link for link in self.mainSoup.find_all(
+        links = ["https://kakuyomu.jp" + link["href"] for link in self.mainSoup.find_all(
             "a", {"class", "widget-toc-episode-episodeTitle"})]
+        return links
 
-        for href in links:
-            episodes_hrefs.append("https://kakuyomu.jp" + href["href"])
-
-        return episodes_hrefs
-
-    # get episode text
-    def get_Story(self, link):
+    def get_contentsDict(self, link):
         soup = BeautifulSoup(requests.get(link).text, "html.parser")
-        episode_structure = [str(html) for html in soup.find_all(
-            "p", {"class", re.compile("(chapterTitle level\d|widget-episodeTitle).*")})]
-        episode_html = [str(html) for html in soup.find_all(
-            "p", id=re.compile("\d+"))]
+        contents_dict = {"chapter": None, "section": None,
+                         "title": None, "episode": None}
 
-        result = self.get_structure("".join(episode_structure))
-        result += "\n" + \
-            self.get_text("\\leavevmode \\\\\n".join(episode_html))
+        chapter = soup.find(
+            "p", {"class", "chapterTitle level1 js-vertical-composition-item"})
+        section = soup.find(
+            "p", {"class", "chapterTitle level2 js-vertical-composition-item"})
+        if chapter:
+            contents_dict["chapter"] = chapter.string
+        if section:
+            contents_dict["section"] = section.string
 
-        return result
+        contents_dict["title"] = soup.find(
+            "p", {"class", "widget-episodeTitle js-vertical-composition-item"}).string
+        contents_dict["episode"] = formatter.html_to_TeX_format("\\leavevmode \\\\\n".join(
+            [str(html) for html in soup.find_all("p", id=re.compile("\d+"))]))
 
-    # get novel structure
-    def get_structure(self, text):
-        result = text
-        result = result.replace(
-            "<p class=\"chapterTitle level1 js-vertical-composition-item\"><span>", "\n\\chapter{")
-        result = result.replace(
-            "<p class=\"chapterTitle level2 js-vertical-composition-item\"><span>", "\n\t\\section{")
-        result = result.replace(
-            "<p class=\"widget-episodeTitle js-vertical-composition-item\">", "\n\t\t\\subsection{")
-        result = re.sub("</span></p>|</p>", "}", result)
-
-        print(result, end="")
-        return result
-
-    # get text for LuaLaTeX format
-    def get_text(self, text):
-        result = text
-        result = result.replace("<ruby><rb>", "\\ruby[g]{")
-        result = result.replace("</rb><rp>（</rp><rt>", "}{")
-        result = result.replace("</rt><rp>）</rp></ruby>", "}")
-        result = result.replace("<em class=\"emphasisDots\">", "\\kenten{")
-        result = result.replace("</em>", "}")
-        result = result.replace("<br/>", "")
-        result = re.sub("<.??span>", "", result)
-        result = re.sub("<.??p.*?>", "", result)
-        result = result.replace("\u3000", "")
-        return result
+        return contents_dict
 
     # create a pdf file by creating and compiling a .tex file.
     def set_novel(self):
@@ -109,26 +86,40 @@ class kakuyomu_LuaLaTeX():
         novel.set_header()
         links = self.get_episodeLinks()
         for link in links:
-            novel.set_text(self.get_Story(link))
-        novel.set_footer()
+            time.sleep(1)
+            contents = self.get_contentsDict(link)
+            if contents["chapter"]:
+                novel.set_chapter(
+                    formatter.escape_to_TeX_format(contents["chapter"]))
+                print("[chapter] -> " + contents["chapter"])
+            if contents["section"]:
+                novel.set_section(
+                    formatter.escape_to_TeX_format(contents["section"]))
+                print("\t[section] -> " + contents["section"])
 
+            novel.set_subsection(
+                formatter.escape_to_TeX_format(contents["title"]))
+            print("\t\t[subsection] -> " + contents["title"])
+
+            novel.set_text(contents["episode"])
+
+        novel.set_footer()
         novel.compile()
 
 
 if __name__ == "__main__":
     # """
-    print("■■■■                                     ■                               ")
-    print("■                          ■■           ■■                               ")
-    print("■                          ■■           ■■■■■■    ■■■■■■■■        ■■     ")
-    print("■            ■             ■■■■■■      ■■   ■            ■        ■      ")
-    print("■            ■          ■■■■    ■     ■■    ■            ■        ■      ")
-    print("■            ■             ■    ■    ■■    ■■            ■       ■■  ■   ")
-    print("■            ■             ■   ■■          ■       ■■■■■■■       ■   ■   ")
-    print("■            ■             ■   ■■         ■■             ■       ■    ■  ")
-    print("■            ■            ■    ■■        ■■              ■      ■    ■■■ ")
-    print("             ■           ■■    ■        ■■        ■■■■■■■■    ■■■■■■■■ ■■")
-    print("             ■          ■■   ■■■       ■                                 ")
-    print("          ■■■■                                         ")
+    print("                 ■                               ")
+    print("   ■■           ■■                               ")
+    print("   ■■           ■■■■■■    ■■■■■■■■        ■■     ")
+    print("   ■■■■■■      ■■   ■            ■        ■      ")
+    print("■■■■    ■     ■■    ■            ■        ■      ")
+    print("   ■    ■    ■■    ■■            ■       ■■  ■   ")
+    print("   ■   ■■          ■       ■■■■■■■       ■   ■   ")
+    print("   ■   ■■         ■■             ■       ■    ■  ")
+    print("  ■    ■■        ■■              ■      ■    ■■■ ")
+    print(" ■■    ■        ■■        ■■■■■■■■    ■■■■■■■■ ■■")
+    print("■■   ■■■       ■                                 ")
     # """
     print("Enter the URL of the main page of the work posted on kakuyomu.")
     download_url = input()
